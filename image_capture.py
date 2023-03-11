81e88f96ab5cebe77f14b43fa536f6a54ba1c8a9
caseylam <casey.lam@tufts.edu>
2023-03-11 17:32:27 -0500

    first draft

---
+++ image_capture.py
@@ -0,0 +1,30 @@
+#import libraries 
+from picamera2 import Picamera2 
+import cv2 as cv 
+import numpy as np
+from libcamera import controls
+import time
+
+picam2 = Picamera2()
+
+#configure the picamera
+picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous}) #sets auto focus mode
+
+picam2.start() #must start the camera before taking any images
+time.sleep(1)
+
+while True:
+    objectName = input("Enter object name: ")
+    if objectName = q:
+        break
+    else:
+        i = 1
+        while True: 
+            cmd = input("Press enter to capture an image.")
+            if cmd == 'q':
+                break
+            else:
+                img_name = objectName + str(i) + '.jpg'
+                picam2.capture_file(img_name) #take image 
+                i = i + 1
+picam2.stop() #stop the picam 
